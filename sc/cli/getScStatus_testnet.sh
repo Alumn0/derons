644c40ed8a21e46926f5d69cbf5cfd4897aa2ea8
Alumno <alumno@homebase.dero>
2024-04-13 10:16:15 -0600

    Changed ip for localhost to public testnet node

--- sc/cli/getScStatus_testnet.sh
+++ sc/cli/getScStatus_testnet.sh
@@ -3,7 +3,7 @@
 BASEDIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source $BASEDIR/scid.dat
 
-output=$(curl -s -X POST http://127.0.0.1:40402/json_rpc -H 'content-type: application/json' -d '{"jsonrpc": "2.0","id": "1","method": "DERO.GetSC","params": {"scid": "'$SCID'","code": false,"variables": true}}' | jq 'del(.result.stringkeys.C)')
+output=$(curl -s -X POST http://ns1.testnet.dero.zone:40402/json_rpc -H 'content-type: application/json' -d '{"jsonrpc": "2.0","id": "1","method": "DERO.GetSC","params": {"scid": "'$SCID'","code": false,"variables": true}}' | jq 'del(.result.stringkeys.C)')
 
 if [ -z "$1" ]; then
     echo "$output"
